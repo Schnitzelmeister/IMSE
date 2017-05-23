6f626b9d1a9bfd9b0add48bc7112c5d1d7b98b3f
unknown <raphael.ornetsmueller@hotmail.com>
2017-05-23 20:53:52 +0200

    Product_and_ProductController_and_Databaseconnection

---
+++ Online_Shopping_System/src/at/ac/ac/univie/imse/SS2017/team1/controller/ProductController.java
@@ -0,0 +1,53 @@
+package at.ac.ac.univie.imse.SS2017.team1.Controller;
+
+import java.util.List;
+
+import at.ac.ac.univie.imse.SS2017.team1.Model.Product;
+
+
+public class ProductController {
+	
+	public Product getProductById(Integer productId){
+		return null;
+	}
+	
+	public List<Product> getProductByName(String productName){
+		return null;
+	}
+	
+	public Product getProductByPrice(Float price){
+		return null;
+	}
+
+	public List<Product> GetAllProduct(){
+		return null;
+	}
+	
+	public void createProduct(String productname, Float price, String description){
+	}
+	
+	public void addProductImage(String image){
+		
+	}
+	
+	public void deleteProductById(Integer productId){
+		
+	}
+	
+	public void deleteImage(String image){
+		
+	}
+	
+	public void updateProductName(String productname){
+		
+	}
+	
+	public void updateProductPrice(Float price){
+		
+	}
+	
+	public void updateProductDescription(String description){
+		
+	}
+	
+}
---
+++ Online_Shopping_System/src/at/ac/ac/univie/imse/SS2017/team1/dao/DaoInterface.java
@@ -0,0 +1,15 @@
+package at.ac.ac.univie.imse.SS2017.team1.Dao;
+
+import java.util.List;
+
+public interface DaoInterface {
+	
+	public <T> void save(T entity);
+
+	public <T> void delete(T entity);
+
+	public <T> T getobject(Class<T> cls, String Id);
+
+	public <T> List<T> getobjects(Class<T> cls);
+	
+}
